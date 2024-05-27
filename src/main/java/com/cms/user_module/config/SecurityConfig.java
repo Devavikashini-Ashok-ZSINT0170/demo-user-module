a69a87569c100938c13e078031e4f106cf304537
devavikashini05 <devavikashini.ashok@zucisystems.com>
2024-05-27 12:16:20 +0530

    changes made in security configuration file

--- src/main/java/com/cms/user_module/config/SecurityConfig.java
+++ src/main/java/com/cms/user_module/config/SecurityConfig.java
@@ -5,44 +5,57 @@ import com.cms.user_module.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity (prePostEnabled = true)
+
+public class SecurityConfig {
 
     @Autowired
     private CustomerUserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeHttpRequests(authCustomizer -> authCustomizer
+                        .requestMatchers(HttpMethod.POST, "/api/customer/add").permitAll()
+                        .requestMatchers("/api/roles/add").hasRole("ADMIN")
+                        .requestMatchers("api/permissions/add").hasRole("ADMIN")
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/customer/add").permitAll() // Allow anyone to add a customer
-                .antMatchers("/api/login").permitAll() // Allow anyone to login
-                .antMatchers("/api/permission/**").hasRole("ADMIN") // Require ADMIN role for permission endpoints
-                .antMatchers("/api/role/**").hasRole("ADMIN") // Require ADMIN role for role endpoints
-                .anyRequest().authenticated()
-                .and()
-                .addFilter(new JwtAuthFilter(authenticationManager()))
-                .formLogin()
-                .and()
-                .httpBasic();
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         // Use a strong password encoder
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userDetailsService;
     }
 }
